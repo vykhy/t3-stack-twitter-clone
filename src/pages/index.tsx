d1e8f96cd851f84d686a7afab6d89c4d4c0d2ea2
vykhy <vikehietoyhoshii@gmail.com>
2023-05-23 11:42:56 +0530

    following and recents tab

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,40 @@
 import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 import InfiniteTweetsList from "~/components/InfiniteTweetsList";
 import NewTweetForm from "~/components/NewTweetForm";
 
 import { api } from "~/utils/api";
 
+const TABS = ["Recent", "Following"] as const;
+
 const Home: NextPage = () => {
+  const [selectedTab, setSelectedTab] =
+    useState<(typeof TABS)[number]>("Recent");
+  const session = useSession();
   return (
     <>
       <header className="sticky-top-0 border-b bg-white pt-2">
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+        {session.status === "authenticated" && (
+          <div className="flex">
+            {TABS.map((tab) => (
+              <button
+                className={`flex-grow p-2 hover:bg-gray-200 focus-visible:bg-gray-200 ${
+                  tab === selectedTab
+                    ? "border-b-4 border-b-blue-500 font-bold"
+                    : ""
+                }`}
+                onClick={() => setSelectedTab(tab)}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
+        )}
       </header>
       <NewTweetForm />
-      <RecentTweets />
+      {selectedTab === "Recent" ? <RecentTweets /> : <FollowingTweets />}
     </>
   );
 };
@@ -19,6 +42,23 @@ const Home: NextPage = () => {
 function RecentTweets() {
   const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
     {},
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+
+  return (
+    <InfiniteTweetsList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage || false}
+      fetchNewTweets={tweets.fetchNextPage}
+    />
+  );
+}
+
+function FollowingTweets() {
+  const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
+    { onlyFollowing: true },
     { getNextPageParam: (lastPage) => lastPage.nextCursor }
   );
 
--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -1,6 +1,9 @@
+import { Prisma } from "@prisma/client";
+import { inferAsyncReturnType } from "@trpc/server";
 import { z } from "zod";
 
 import {
+  createTRPCContext,
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
@@ -10,50 +13,32 @@ export const tweetRouter = createTRPCRouter({
   infiniteFeed: publicProcedure
     .input(
       z.object({
+        onlyFollowing: z.boolean().optional(),
         limit: z.number().optional(),
         cursor: z.object({ id: z.string(), createdAt: z.date() }).optional(),
       })
     )
-    .query(async ({ input: { limit = 10, cursor = undefined }, ctx }) => {
-      const currentUserId = ctx.session?.user.id;
-
-      const data = await ctx.prisma.tweet.findMany({
-        take: limit + 1,
-        cursor: cursor ? { createdAt_id: cursor } : undefined,
-        orderBy: [{ createdAt: "desc" }, { id: "desc" }],
-        select: {
-          id: true,
-          content: true,
-          createdAt: true,
-          _count: { select: { likes: true } },
-          likes:
-            currentUserId == null
-              ? false
-              : { where: { userId: currentUserId } },
-          user: {
-            select: { name: true, id: true, image: true },
-          },
-        },
-      });
-      let nextCursor: typeof cursor | undefined;
-      if (data.length > limit) {
-        const nextItem = data.pop();
-        if (nextItem != null) {
-          nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
-        }
+    .query(
+      async ({
+        input: { limit = 10, onlyFollowing = false, cursor = undefined },
+        ctx,
+      }) => {
+        const currentUserId = ctx.session?.user.id;
+        return await getInfiniteTweets({
+          limit,
+          ctx,
+          cursor,
+          whereClause:
+            currentUserId == null || !onlyFollowing
+              ? undefined
+              : {
+                  user: {
+                    followers: { some: { id: currentUserId } },
+                  },
+                },
+        });
       }
-      return {
-        tweets: data.map((tweet) => ({
-          id: tweet.id,
-          content: tweet.content,
-          createdAt: tweet.createdAt,
-          likeCount: tweet._count.likes,
-          user: tweet.user,
-          likedByMe: tweet.likes?.length > 0,
-        })),
-        nextCursor,
-      };
-    }),
+    ),
   create: protectedProcedure
     .input(z.object({ content: z.string() }))
     .mutation(async ({ input: { content }, ctx }) => {
@@ -80,3 +65,58 @@ export const tweetRouter = createTRPCRouter({
       }
     }),
 });
+
+async function getInfiniteTweets({
+  whereClause,
+  ctx,
+  limit,
+  cursor,
+}: {
+  whereClause?: Prisma.TweetWhereInput;
+  limit: number;
+  cursor:
+    | {
+        id: string;
+        createdAt: Date;
+      }
+    | undefined;
+  ctx: inferAsyncReturnType<typeof createTRPCContext>;
+}) {
+  const currentUserId = ctx.session?.user.id;
+
+  const data = await ctx.prisma.tweet.findMany({
+    take: limit + 1,
+    cursor: cursor ? { createdAt_id: cursor } : undefined,
+    orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+    where: whereClause,
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+      _count: { select: { likes: true } },
+      likes:
+        currentUserId == null ? false : { where: { userId: currentUserId } },
+      user: {
+        select: { name: true, id: true, image: true },
+      },
+    },
+  });
+  let nextCursor: typeof cursor | undefined;
+  if (data.length > limit) {
+    const nextItem = data.pop();
+    if (nextItem != null) {
+      nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
+    }
+  }
+  return {
+    tweets: data.map((tweet) => ({
+      id: tweet.id,
+      content: tweet.content,
+      createdAt: tweet.createdAt,
+      likeCount: tweet._count.likes,
+      user: tweet.user,
+      likedByMe: tweet.likes?.length > 0,
+    })),
+    nextCursor,
+  };
+}
