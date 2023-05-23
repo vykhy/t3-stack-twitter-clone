a5012756f7a6e4d62035b5328bf04212eebd4fea
vykhy <vikehietoyhoshii@gmail.com>
2023-05-23 11:19:27 +0530

    new tweet state update

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -20,6 +20,7 @@ function Form() {
   const session = useSession();
   const [inputValue, setInputValue] = useState("");
   const textAreaRef = useRef<HTMLTextAreaElement>();
+  const trpcUtils = api.useContext();
 
   const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
     updateTextAreaHeight(textArea);
@@ -31,7 +32,37 @@ function Form() {
   }, [inputValue]);
 
   const createTweet = api.tweet.create.useMutation({
-    onSuccess: (newTweet) => setInputValue(""),
+    onSuccess: (newTweet) => {
+      setInputValue("");
+
+      if (session.status !== "authenticated") return;
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (!oldData || oldData.pages[0] == null) return;
+
+        const newCacheTweet = {
+          ...newTweet,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data.user.id,
+            name: session.data.user.name || null,
+            image: session.data.user.image || null,
+          },
+        };
+
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              tweets: [newCacheTweet, ...oldData.pages[0].tweets],
+            },
+            ...oldData.pages.slice(1),
+          ],
+        };
+      });
+    },
   });
 
   function handleSubmit(e: FormEvent) {
