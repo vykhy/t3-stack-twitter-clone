2928438797477bf4f48a26219eafc289c797430b
vykhy <vikehietoyhoshii@gmail.com>
2023-05-23 09:38:02 +0530

    fallback profile image icon

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -12,7 +12,7 @@ function ProfileImage({ src, className = "" }: ProfileImageProps) {
     <div
       className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}
     >
-      {src != null ? (
+      {src == null ? (
         <VscAccount className="h-full w-full" />
       ) : (
         <Image src={src} alt="Profile Image" quality={100} fill />
